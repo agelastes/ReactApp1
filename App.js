ac886a53aaa50d38f51e915d7b4cb4dab247db37
agelastes <erstedy1517@mail.ru>
2018-07-26 21:23:02 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,37 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  f(){
-        fetch('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
+
+    flag = false
+c_name
+c_temp
+
+    constructor(props){
+        super(props);
+        this.state = {lobster: 0};
+    }
+
+    f(){
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=e090edb163b2c70e2b6b6f078f302a79&units=metrice090edb163b2c70e2b6b6f078f302a79')
             .then(function(response) {
                 return response.json();
             })
             .then(function(json) {
-                alert(json.name);
+                this.c_name = json.name;
+                this.c_temp = json.main.temp;
+                this.setState({lobster: this.state.lobster + 1});
+                return this.c_name , this.c_temp;
             })
     }
 
-    state = {
-        valid: true
-    };
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"></img>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <form>
-              <input align="center" className="Input-1" onChange={this.state.valid = true} ></input>
-              <input className="Input-2" type = "Button" value="Нажми на меня" onClick={this.f()}></input>
-          </form>
-      </div>
-    );
-  }
-
+    render(){
+    if((!this.c_name)&&(this.flag == true)) return null;
+    if(this.flag == true) alert("Working!");
+        alert("1ddf");
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"></img>
+                    <h1 className="App-title">app2</h1>
+                </header>
+                <form>
+                    <h2>Введите название города чтобы получить прогноз</h2>
+                    <input align="center" className="Input-1"></input>
+                </form>
+                <form>
+                    <input className="Input-2" type = "Button" value="Нажми на меня" onClick={this.f.bind(this)} onMouseUp={this.flag = true}></input>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
